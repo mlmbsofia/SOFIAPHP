8ad1e99acede5b3013795b8df23fed6946da2dc4
sofiramoss <204217590+mlmbsofia@users.noreply.github.com>
2025-10-27 15:49:30 -0300

    triste

--- funcao/contartempo.php
+++ funcao/contartempo.php
@@ -1,17 +1,43 @@
 <?php
 
-function contartempo(string $date) : string
+function contarTempo(string $date) : string
 {
 
     $agora = strtotime(date('Y-m-d H:i:s'));
     $tempo = strtotime($date);
 
-    echo ($agora);
+    echo ('Data do pc '. $agora);
     echo "<hr>";
-    echo ($tempo);
+    echo ('Data digitada '. $tempo);
 
     $diferenca = $tempo - $agora;
+    echo "<hr>";
+    echo ('Diferença '. $diferenca);
+
+    $segundos = ($diferenca);
+    $minutos= round($diferenca / 60);
+    $horas = round($diferenca / 3600);
+    $dias = round($diferenca / 86400);
+    $semanas = round($diferenca / 604800);
+    $meses = round($diferenca / 2419200);
+    $anos = round($diferenca / 31536000);
+
+    if ($segundos >= 60) {
+        return 'Agora ';
+    }elseif ($minutos >= 60) {
+        return $minutos == 1 ? ' Há um minuto' : "Há" . $minutos . " minutos";
+    }elseif ($horas >= 24) {
+        return $horas == 1 ? ' Há 1 hora' : "Há" . $horas . " horas";
+    }elseif ($dias >= 7) {
+        return $dias == 1 ? ' Há 1 dia' : 'Há' . $dias . ' dias';
+    }elseif ($semanas >= 4) {
+        return $semanas == 1 ? 'há 1 semana' : 'Há' . $meses . " semanas";
+    }elseif ($meses >= 12) {
+        return $meses == 1 ? 'há 1 mês' : 'Há' . $meses . 'meses';
+    }else{
+        return $anos == 1 ? 'há 1 ano' : 'há' . $anos . " anos";
+    }
 
 }
-
-contartempo( 2025-10-27 14:10:20)
+echo '<hr>';
+echo contarTempo('2025-10-27 15:12:00');
