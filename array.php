e58a926e06df80111d80faa7ef0f687ae7215abd
ViniciusbcMarques <morenopagode13@gmail.com>
2025-11-03 11:30:07 -0300

    aah

--- array.php
+++ array.php
@@ -1,3 +1,34 @@
 <?php
 
 $meses = array();
+$mes =[
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+foreach ($mes as $key => $value) {
+    echo $key . '=>' . $value . '<br>';
+}
+echo '<hr>';
+echo 'VAR_DUMP';
+var_dump($mes);
+echo '<hr>';
+echo 'Posição 1 do array MÊS'. '<br>';
+echo $mes[1];
+echo '<hr>';
+
+function dataAtual(): string
+{
+    $diaMes = date('d');
+    $diaSemana = date('w');
+    $mes = date('n');
+    $ano = date('Y');
+
+    $nomeDosMeses = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+    $nomeDiaDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+    $dataFormatada = $nomeDiaDaSemana[$diaSemana] . ',' . $diaMes . ',' . 'de';
+}
+
+
